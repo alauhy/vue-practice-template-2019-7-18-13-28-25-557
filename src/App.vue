bbe6e7d1c649609aa8ea7778696fc121f7169fd2
alauhy <751961605@qq.com>
2019-07-19 17:39:12 +0800

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <div id="app">
- <Count v-for="n in 3"></Count>
-  </div>
+    <div id="app">
+        <input type="number" v-model="counterNum">
+        <CounterGroup :counterNum="parseInt(counterNum)"></CounterGroup>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Count from './components/Count.vue'
-export default {
-  name: 'app',
-  components: {
-   Count
-  },
-  data(){
+    import HelloWorld from './components/HelloWorld.vue'
+    import Count from './components/Count.vue'
+    import CounterGroup from './components/CounterGroup'
 
-  }
-}
+    export default {
+        name: 'app',
+
+        components: {
+            CounterGroup
+        },
+        data() {
+            return {
+                counterNum: 2
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/CounterGroup.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <div id="CounterGroup">
+        <Count v-for="count in counterNum" :key="count"></Count>
+    </div>
+
+
+</template>
+
+<script>
+    import Count from './Count'
+
+    export default {
+
+        name: "CounterGroup",
+        props:{
+            counterNum:Number
+        },
+        components: {
+            Count
+        },
+        data() {
+            return {
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
